c2e5724d96fcb7042a3f2360d62399bd3aeaedf0
jiamen <2731275645@qq.com>
2021-04-30 15:51:43 +0800

    ndt

--- include/points_downsample.h
+++ include/points_downsample.h
@@ -2,7 +2,43 @@
 // Created by zlc on 2021/4/25.
 //
 
-#ifndef NDT_LOCALIZER_POINTS_DOWNSAMPLE_H
-#define NDT_LOCALIZER_POINTS_DOWNSAMPLE_H
+#ifndef _NDT_LOCALIZER_POINTS_DOWNSAMPLE_H_
+#define _NDT_LOCALIZER_POINTS_DOWNSAMPLE_H_
 
-#endif //NDT_LOCALIZER_POINTS_DOWNSAMPLE_H
+
+static pcl::PointCloud<pcl::PointXYZ> removePointByRange(pcl::PointCloud<pcl::PointXYZ> scan, double min_range, double max_range)
+{
+    pcl::PointCloud<pcl::PointXYZ> narrowed_scan;
+    narrowed_scan.header = scan.header;
+
+#if 1     //  This error handling should be detemind.
+    if (min_range>=max_range)
+    {
+        ROS_ERROR_ONCE("min_range>=max_range @");
+        return scan;
+    }
+#endif
+
+    double square_min_range = min_range * min_range;
+    double square_max_range = max_range * max_range;
+
+    for (pcl::PointCloud<pcl::PointXYZ>::const_iterator iter = scan.begin(); iter != scan.end(); ++ iter)
+    {
+        const pcl::PointXYZ& p = *iter;
+        // p.x = iter->x;
+        // p.y = iter->y;
+        // p.z = iter->z;
+        // p.intensity = iter->intensity;
+        double square_distance = p.x * p.x + p.y * p.y;
+
+        if (square_min_range<=square_distance && square_distance<=square_max_range)
+        {
+            narrowed_scan.points.push_back(p);
+        }
+    }
+
+    return narrowed_scan;
+}
+
+
+#endif // _NDT_LOCALIZER_POINTS_DOWNSAMPLE_H_
--- nodes/voxel_grid_filter.cpp
+++ nodes/voxel_grid_filter.cpp
@@ -2,3 +2,52 @@
 // Created by zlc on 2021/4/25.
 //
 
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/voxel_grid.h>             // 体素滤波
+
+#include "points_downsample.h"
+
+#define MAX_MEASUREMENT_RANGE 120.0
+
+ros::Publisher filter_points_pub;               // ROS发布消息
+
+// Leaf size of VoxelGrid filter
+static double voxel_leaf_size = 2.0;
+
+static bool _output_log = false;
+static std::ofstream ofs;
+static std::string filename;
+
+static std::string POINTS_TOPIC;
+static double measurement_range = MAX_MEASUREMENT_RANGE;
+
+
+static void scan_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
+{
+    pcl::PointCloud<pcl::PointXYZ> scan;
+    pcl::fromROSMsg(*input, scan);
+
+    if (measurement_range != MAX_MEASUREMENT_RANGE)
+    {
+        scan = removePointByRange(scan, 0, measurement_range);
+    }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr scan_ptr(new pcl::PointCloud<pcl::PointXYZ>(scan));
+    pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_scan_ptr(new pcl::PointCloud<pcl::PointXYZ>());
+
+    sensor_msgs::PointCloud2 filtered_msg;
+
+    // if voxel_leaf_size < 0.1 voxel_grid_filter
+
+}
+
+
+
+
+
+
+
